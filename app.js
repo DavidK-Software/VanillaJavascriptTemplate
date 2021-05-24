42ad0eca173f52a8e42577ae73c2970f544d8c21
David <David.Kissell@outlook.com>
2021-05-23 22:25:03 -0500

    Added simple SPA type code

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 /* Your code here */
+
+/* Page navigation */
+
+function showPage1() {
+    console.log("function showPage1()");
+    document.querySelector("#page-1").style.display = "";
+    document.querySelector("#page-2").style.display = "none";
+    document.querySelector("#page-3").style.display = "none";
+}
+
+function showPage2() {
+    console.log("function showPage2()");
+    document.querySelector("#page-1").style.display = "none";
+    document.querySelector("#page-2").style.display = "";
+    document.querySelector("#page-3").style.display = "none";
+}
+
+function showPage3() {
+    console.log("function showPage3()");
+    document.querySelector("#page-1").style.display = "none";
+    document.querySelector("#page-2").style.display = "none";
+    document.querySelector("#page-3").style.display = "";
+}
+
+showPage1();
+
 console.log("App javascript run");
